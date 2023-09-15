ab7a559904fb37a87d7648687addb852e5c37259
kurves <kavssimply@gmail.com>
2023-09-15 21:59:06 +0300

    tests for rest app

--- crudapp/tests.py
+++ crudapp/tests.py
@@ -1,32 +1,39 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework import status
 from django.urls import reverse
 
 # Create your tests here.
 from .models import Person
 
-class PersonCrudTests(TestCase):
+class PersonAPITestCase(TestCase):
     ## Test for creating item
     def setup(self):
-        self.Person.objects.create(name = "Mark Essien",details='Software Engineer')
+        self.client=APIClient()
+        self.person_data={"name":"Mark Essien","details":"Software Engineer"}
+        self.Person= Person.objects.create(**self.person_data)
 
-    def test_new_person_view(self):
-        res= self.client.post(reverse("crudapp:new_person"),{'name':"Mark Essien","details":"Software Engineer"})
-        self.assertEqual(res.status_code,302)
-        self.assertTrue(Person.objects.filter(name='Mark Essien').exists())
+    def test_create_person(self):
+        self.person_data={"name":"Mark Essien","details":"Software Engineer"}
+        response=self.client.post('/api/', self.person_data,format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        
+    def test_list_person(self):
+        response = self.client.get('/api/') 
+        self.assertEqual(response.status_code, status.HTTP_200_OK) 
+          
+
+    def test_update_person(self):
+            updated_person={"name":"Maureen Mwenswa","details":"Civil engineer"}
+            response=self.client.put('/api/<id>/', updated_person,format='json')
+            self.assertEqual(updated_person["name"], "Maureen Mwenswa")
+           
 
 
-    def test_person_view(self):
-        res=self.client.get(reverse("crudapp:search_person",args=[id])) 
-        self.assertEqual(res.status_code, 200)
-        #self.assertContains(res,"Mark Essien")
+    def test_delete_person(self):
+        response=self.client.delete('api/<int:pk>/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertFalse(Person.objects.filter(name="Mark Essien").exists()) 
+        self.assertEqual(Person.objects.count(),0)
 
-    def test_delete_person_view(self):
-        res=self.client.post(reverse('crudapp:update_person',args=[id]))
-        self.assertEqual(res.status_code,200)
-        self.assertFalse(Person.objects.filter(name="Mark Essien").exists())  
 
-    def test_update_person_view(self):
-        res=self.client.post(reverse('crudapp:new_person'),{'name':'new person','details':'This is a new person'})    
-        self.assertEqual(res.status_code,302)
-        updated_name=Person.objects.get()
-        self.assertNotEqual(updated_name, 'new person' )   
