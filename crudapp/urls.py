4d92618a5516af6d08752e6ebac47b3dbbc7f897
kurves <kavssimply@gmail.com>
2023-09-16 12:12:46 +0300

    update settings.py

--- crudapp/urls.py
+++ crudapp/urls.py
@@ -9,7 +9,7 @@ app_name="crudapp"
 urlpatterns=[
     #home page
     path('', views.index, name='index'),
-    path('api/',views.PersonListView.as_view(),name='Person-list'),
+    path('api',views.PersonListView.as_view(),name='Person-list'),
     path('api/<int:pk>',views.CreateUpdateView.as_view(),name='create-update'),
     path('api/<str:name>', views.PersonRetrieveByName.as_view(),name="Retrieve-Person-by-name")
    
